b59eb3b95d8bc069af173155882cd43d554dfb78
Bryan Larson <b32147@gmail.com>
2018-04-13 17:45:03 -0400

    Simplified build script

--- build.sh
+++ build.sh
@@ -2,47 +2,26 @@
 
 # Check for a testing app
 if [ -z ${1+x} ]; then
-  exit 0
-else
-    # Determine where app sources are located
-    APPS_DIR=${PPM_APPS_DIR:-"./apps/"}
+    echo "The app identifier is required"
+    exit 1
+elif [ -z ${2+x} ]; then
+    echo "The path to the app's build context is required"
+    exit 1
+fi
 
-    cat > ./docker-compose.override.yml <<EOL
+# Set the image name
+IMAGE="${3:-ppmtest}/$1"
+
+# Replace its image
+echo "Building $1 -> $IMAGE"
+
+# Build it
+#docker build -t "$IMAGE" "$2"
+
+# Initialize the override file
+cat > ./docker-compose.override.yml <<EOL
 version: '2.1'
 services:
-EOL
-    for i in $(echo $1 | sed "s/,/ /g")
-    do
-
-        # Make sure the source exists
-        CONTEXT="$APPS_DIR/$i"
-        if [ ! -f "$CONTEXT" ]; then
-            echo "$i - App source directory does not exist: $CONTEXT" >&2
-        else
-
-            # Set the image name
-            IMAGE="ppmtest/$i:test"
-
-            # Replace its image
-            echo "Building $i -> $IMAGE"
-
-            # Build it
-            docker build -t "$IMAGE" "$CONTEXT"
-
-            # Append the override
-            cat >> ./docker-compose.override.yml <<EOL
-  $i:
+  $1:
     image: $IMAGE
 EOL
-        fi
-    done
-fi
-
-# Ensure override is not empty
-if [[ $(wc -l <./docker-compose.override.yml) -lt 4 ]]; then
-
-    # Remove it
-    echo "No apps were build, removing docker-compose.override.yml" >&2
-    rm ./docker-compose.override.yml
-
-fi
