f6d86100db63d789d5b1fd2791ff08b94e912813
Bryan Larson <b32147@gmail.com>
2018-04-13 13:33:15 -0400

    Fixed cleanup and artifact handling

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,6 +5,10 @@ pipeline {
             defaultValue: '',
             description: 'The tests to run against the stack',
             name: 'PPM_TEST_TEST')
+        text(
+            defaultValue: 'artifacts',
+            description: 'The directory to collect output artifacts in',
+            name: 'PPM_TEST_ARTIFACTS')
     }
 
     stages {
@@ -29,9 +33,12 @@ pipeline {
 
     post {
         always {
+            // Collect logs and such
+            archiveArtifacts artifacts: '${PPM_TEST_ARTIFACTS}/*', fingerprint: true
+
             // Clean up Docker and stack
-            sh("docker-compose down -v --remove-orphans")
-            sh("docker-compose rm -v -f -s")
+            sh("docker-compose -p ${JOB_NAME}-${BUILD_ID} down -v --remove-orphans")
+            sh("docker-compose -p ${JOB_NAME}-${BUILD_ID} rm -v -f -s")
 
             // Purge workspace
             deleteDir()
--- test.sh
+++ test.sh
@@ -3,47 +3,40 @@
 # Check for a specific test
 export PPM_TEST_TEST="$1"
 
-
 # Get the output archive path
-OUTPUT_PATH=${PPM_TEST_OUTPUT_PATH:-"ppm-test-output.tar.gz"}
-
-# Get the latest and greatest
-docker-compose pull --parallel
-
-# Ensure the stack is not already running
-docker-compose down -v --remove-orphans
-docker-compose rm -v -f -s
-
-# Output link to monitoring
-echo "Follow tests at: http://localhost:4444/grid/admin/live#"
+ARTIFACTS_PATH=${3:-artifacts}
 
 # RGet the current date and create a directory for collecting logs
 today=`date +%Y%m%d_%H%M%S`
-mkdir -p "./$today"
+mkdir -p "./$ARTIFACTS_PATH"
 
 # Determine what to call the stack
 PPM_PROJECT_NAME=${2:-$today}
 
+# Get the latest and greatest
+docker-compose -p $PPM_PROJECT_NAME pull --parallel
+
+# Ensure the stack is not already running
+docker-compose -p $PPM_PROJECT_NAME down -v --remove-orphans
+docker-compose -p $PPM_PROJECT_NAME rm -v -f -s
+
+# Output link to monitoring
+echo "Follow tests at: http://18.204.95.224:4444/grid/admin/live#"
+
 # Run the stack and get exit code from tests (videos not exporting)
-docker-compose -p $PPM_PROJECT_NAME up --exit-code-from test --abort-on-container-exit | tee "./$today/test-output-$today.log"
+docker-compose -p $PPM_PROJECT_NAME up --exit-code-from test --abort-on-container-exit | tee "./$ARTIFACTS_PATH/test-output-$today.log"
 RESULT=${PIPESTATUS[0]}
+
+# Collect files
+docker-compose -p $PPM_PROJECT_NAME logs -t > "$ARTIFACTS_PATH/test-logs-$today.log"
 
 if [ $RESULT -ne 0 ]; then
 
     echo 'Error: Tests failed!' >&2
 
-    # Collect files
-    docker-compose logs -t > "$today/test-logs-$today.log"
-
-    # Tar the output directory
-    tar czf "$OUTPUT_PATH" "$today"
-
 else
 
     echo "Tests succeeded!"
-
-    # Tar the output directory
-    tar czf "$OUTPUT_PATH" "$today"
 
 fi
 
